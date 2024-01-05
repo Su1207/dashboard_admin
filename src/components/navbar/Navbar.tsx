4e558fa0e1e1faaac037a86dbc0d53bbc48861a4
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-01-05 12:45:13 +0530

    next commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,14 @@
 import "./navbar.scss";
+import { RiMenu3Line } from "react-icons/ri";
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
+        <div className="hamburger">
+          <input type="checkbox" />
+          <RiMenu3Line />
+        </div>
         <img src="/logo.svg" alt="" />
         <span>Admin Dashboard</span>
       </div>
