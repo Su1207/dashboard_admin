c4c6e8122816ec114866dde34ce4355b7437038e
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-02-15 00:46:23 +0530

    next commit

--- src/pages/games/Games.tsx
+++ src/pages/games/Games.tsx
@@ -3,10 +3,12 @@ import GamesDetails from "../../components/GamesMarket/GamesDetails/GamesDetails
 import { useAuth } from "../../components/auth-context";
 import "./games.scss";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSubAuth } from "../../components/subAdmin-authContext";
 import { usePermissionContext } from "../../components/AdmissionPermission";
 import { Navigate } from "react-router-dom";
+import { database } from "../../firebase";
+import { get, ref } from "firebase/database";
 
 const Games = () => {
   const [addGame, setAddGame] = useState(false);
@@ -16,15 +18,37 @@ const Games = () => {
   };
 
   const { isAuthenticated } = useAuth();
-  const { isSubAuthenticated } = useSubAuth();
-  const { permissions } = usePermissionContext();
+  const { isSubAuthenticated, user } = useSubAuth();
+  const { switched } = usePermissionContext();
+  const [permission, setPermission] = useState<boolean>();
+
+  if (!isAuthenticated && !isSubAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  useEffect(() => {
+    try {
+      const permissionRef = ref(
+        database,
+        `ADMIN/SUB_ADMIN/${user?.ID}/PERMISSIONS/USERS`
+      );
+
+      get(permissionRef).then((snapshot: any) => {
+        if (snapshot.exists()) {
+          setPermission(snapshot.val());
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [switched]);
 
   if (!isAuthenticated && !isSubAuthenticated) {
     return <Navigate to="/login" />;
   }
   return (
     <>
-      {isAuthenticated || (isSubAuthenticated && permissions?.MARKET) ? (
+      {isAuthenticated || (isSubAuthenticated && permission) ? (
         <div className="games">
           <div className="games_title">
             <h1 className="market">Market</h1>
