1b9776d4b2ecf3f29921b7b7b580cdecdf1197a8
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-02-16 02:39:33 +0530

    next commit

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -3,7 +3,9 @@ import "./user.scss";
 import UserDetail from "../../components/UserDetail/UserDetail";
 import { useAuth } from "../../components/auth-context";
 import { useSubAuth } from "../../components/subAdmin-authContext";
-import { usePermissionContext } from "../../components/AdmissionPermission";
+import { useEffect, useState } from "react";
+import { database } from "../../firebase";
+import { onValue, ref } from "firebase/database";
 
 const User = () => {
   //this way ensure that useParams never return undefined value
@@ -12,16 +14,35 @@ const User = () => {
   console.log(userId);
 
   const { isAuthenticated } = useAuth();
-  const { isSubAuthenticated } = useSubAuth();
-  const { permissions } = usePermissionContext();
+  const { isSubAuthenticated, user } = useSubAuth();
+  const [permission, setPermission] = useState<boolean>();
 
   if (!isAuthenticated && !isSubAuthenticated) {
     return <Navigate to="/login" />;
   }
 
+  useEffect(() => {
+    try {
+      const permissionRef = ref(
+        database,
+        `ADMIN/SUB_ADMIN/${user?.ID}/PERMISSIONS/USERS`
+      );
+
+      const unsub = onValue(permissionRef, (snapshot) => {
+        if (snapshot.exists()) {
+          setPermission(snapshot.val());
+        }
+      });
+
+      return () => unsub();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <div>
-      {isAuthenticated || (isSubAuthenticated && permissions?.USERS) ? (
+      {isAuthenticated || (isSubAuthenticated && permission) ? (
         <UserDetail userId={Number(userId)} />
       ) : (
         <p>No access to this data</p>
