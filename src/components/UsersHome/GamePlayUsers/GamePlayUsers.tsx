e27c31080ec9fca726c51e3af7c58a3f5fcb8082
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-02-19 23:13:55 +0530

    next commit

--- src/components/UsersHome/GamePlayUsers/GamePlayUsers.tsx
+++ src/components/UsersHome/GamePlayUsers/GamePlayUsers.tsx
@@ -7,6 +7,7 @@ type UsersGameDataType = {
   phone: string;
   name: string;
   markets: string[];
+  games: Set<string>;
 };
 
 const GamePlayUsers = () => {
@@ -17,8 +18,8 @@ const GamePlayUsers = () => {
 
   const currentDate = new Date();
   const year = currentDate.getFullYear();
-  // const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-  // const day = currentDate.getDate();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+  const day = currentDate.getDate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,13 +37,14 @@ const GamePlayUsers = () => {
             const gameName: string[] = [];
             let Name: string = "";
             let phone: string = "";
+            const games: Set<string> = new Set();
 
             const gamesnapshot = usersSnapshot
               .child("BID")
               .child("DATE WISE")
               .child(`${year}`)
-              .child("01")
-              .child("27");
+              .child(`${month}`)
+              .child(`${day}`);
 
             if (gamesnapshot.exists()) {
               gamesnapshot.forEach((gameSnapshot: any) => {
@@ -55,6 +57,10 @@ const GamePlayUsers = () => {
                   gameName.push(marketName);
                   Name = name;
                   phone = number;
+
+                  gameSnapshot.forEach((timesnap: any) => {
+                    games.add(timesnap.val().GAME);
+                  });
                 }
               });
 
@@ -62,6 +68,7 @@ const GamePlayUsers = () => {
                 phone: phone,
                 name: Name,
                 markets: gameName,
+                games: games,
               });
             }
           });
@@ -77,6 +84,8 @@ const GamePlayUsers = () => {
     fetchData();
   }, []);
 
+  console.log(usersGameData);
+
   return (
     <div className="gamePlay_users">
       <h4>TODAY GAMEPLAY USERS</h4>
@@ -86,22 +95,33 @@ const GamePlayUsers = () => {
       ) : usersGameData ? (
         <div className="users_list">
           {usersGameData.map((data) => (
-            <div className="users_game_data" key={data.phone}>
-              <div className="user_data">
-                <img src="user.png" alt="" className="user_img_icon" />
-                <div className="user_detail">
-                  <div className="users_name">{data.name}</div>
-                  <div className="user_phone">+91{data.phone}</div>
+            <div className="users_game_data_container" key={data.phone}>
+              <div className="users_game_data">
+                <div className="user_data">
+                  <img src="user.png" alt="" className="user_img_icon" />
+                  <div className="user_detail">
+                    <div className="users_name">{data.name}</div>
+                    <div className="user_phone">+91{data.phone}</div>
+                  </div>
+                </div>
+
+                <div className="game_data">
+                  <ul className="games-list">
+                    {data.markets.map((game) => (
+                      <li key={game}>{game}</li>
+                    ))}
+                  </ul>
+                </div>
+
+                <div className="game_data">
+                  <ul className="games-list">
+                    {[...data.games].map((game) => (
+                      <li key={game}>{game}</li>
+                    ))}
+                  </ul>
                 </div>
               </div>
-
-              <div className="game_data">
-                <ul className="games-list">
-                  {data.markets.map((game) => (
-                    <li key={game}>{game}</li>
-                  ))}
-                </ul>
-              </div>
+              <div className="bottom_border"></div>
             </div>
           ))}
         </div>
